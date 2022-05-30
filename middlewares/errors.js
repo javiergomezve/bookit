84da06b79b8ae22ecd8ef8a40f0017e17f39ad6c
Javier Gomez <javiergomezve@gmail.com>
2022-05-29 19:50:09 -0500

    [ADD] Handling mongoose errors

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -6,6 +6,18 @@ const handleError = (err, req, res, next) => {
     let error = { ...err };
     error.message = err.message;
 
+    // wrong mongoose object id error
+    if (err.name === 'CastError') {
+        const message = `Resource not found. Invalid: ${err.path}`;
+        error = new ErrorHandler(message, 400);
+    }
+
+    // handling mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value => value.message);
+        error = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         error,
