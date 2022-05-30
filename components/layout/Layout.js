454ee4b699780ee4c376974bc69470074d01dd48
Javier Gomez <javiergomezve@gmail.com>
2022-05-30 17:26:53 -0500

    [ADD] Home template

---
+++ components/layout/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({ children, title = 'Book best hotel in the web!' }) => {
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+
+                <meta charSet="UTF-8" />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0,width=device-width"
+                />
+            </Head>
+
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    );
+};
+
+export default Layout;
